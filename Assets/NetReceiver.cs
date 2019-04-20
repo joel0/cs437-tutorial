f8944244e3a5f2a5151f70bb94feacf0eafbbee3
Joel May <joel.may@live.com>
2019-04-19 21:33:13 -0500

    UDP demo

---
+++ Assets/NetReceiver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetReceiver : MonoBehaviour
+{
+    Socket mUdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    public int ListenPort = 1337;
+    private float mTargetX = 0;
+    private float mTargetY = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SocketAsyncEventArgs socketArgs = new SocketAsyncEventArgs();
+        mUdpSocket.Bind(new IPEndPoint(IPAddress.Any, ListenPort));
+        socketArgs.SetBuffer(new byte[8], 0, 8);
+        socketArgs.Completed += new EventHandler<SocketAsyncEventArgs>(PacketReceived);
+        mUdpSocket.ReceiveAsync(socketArgs);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(mTargetX, mTargetY);
+    }
+
+    void PacketReceived(object o, SocketAsyncEventArgs args)
+    {
+        Socket socket = (Socket)o;
+        mTargetX = BitConverter.ToSingle(args.Buffer, 0);
+        mTargetY = BitConverter.ToSingle(args.Buffer, 4);
+        socket.ReceiveAsync(args);
+    }
+}
---
+++ Assets/NetSender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetSender : MonoBehaviour
+{
+    Socket mUdpSend = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    public string remoteHost = "127.0.0.1";
+    public int remotePort = 1337;
+    IPEndPoint mRemoteEndPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRemoteEndPoint = new IPEndPoint(IPAddress.Parse(remoteHost), remotePort);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SendLocation(transform.position.x,
+            transform.position.y);
+    }
+
+    void SendLocation(float x, float y)
+    {
+        byte[] buffer = new byte[8];
+        BitConverter.GetBytes(x).CopyTo(buffer, 0);
+        BitConverter.GetBytes(y).CopyTo(buffer, 4);
+        mUdpSend.SendTo(buffer, mRemoteEndPoint);
+    }
+}
