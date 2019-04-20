efe8ba854b247a59fa2d407d5e75b2d75368fee6
Joel May <joel.may@live.com>
2019-04-20 00:56:44 -0500

    TCP

---
+++ Assets/Scripts/TcpConnection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class TcpConnection : MonoBehaviour
+{
+    public GameObject PlayerLocal;
+    public GameObject PlayerRemote;
+    public GameObject TextConnected;
+    public GameObject TextConnectFailed;
+    public string RemoteHost = "127.0.0.1";
+    public int RemotePort = 1337;
+    bool mConnected = false;
+    bool mConnectionError = false;
+    float mTargetX = 0;
+    float mTargetY = 0;
+
+    Socket mSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextConnected.SetActive(false);
+        TextConnectFailed.SetActive(false);
+        SocketAsyncEventArgs socketArgs = new SocketAsyncEventArgs();
+        socketArgs.Completed += new System.EventHandler<SocketAsyncEventArgs>(SocketConnected);
+        socketArgs.RemoteEndPoint = new IPEndPoint(IPAddress.Parse(RemoteHost), RemotePort);
+        socketArgs.SetBuffer(new byte[8], 0, 8);
+        mSocket.ConnectAsync(socketArgs);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextConnected.SetActive(mConnected);
+        TextConnectFailed.SetActive(mConnectionError);
+
+        SendLocation();
+        UpdatePlayerRemote();
+    }
+
+    void OnDestroy()
+    {
+        mSocket.Close();
+    }
+
+    void SocketConnected(object o, SocketAsyncEventArgs args)
+    {
+        Socket s = (Socket)o;
+        mConnected = s.Connected;
+        mConnectionError = !mConnected;
+
+        if (args.LastOperation == SocketAsyncOperation.Receive)
+        {
+            mTargetX = BitConverter.ToSingle(args.Buffer, 0);
+            mTargetY = BitConverter.ToSingle(args.Buffer, 4);
+        }
+
+        if (s.Connected)
+        {
+            s.ReceiveAsync(args);
+        }
+    }
+
+    void SendLocation()
+    {
+        if (!mSocket.Connected)
+        {
+            return;
+        }
+        byte[] buffer = new byte[8];
+        float x = PlayerLocal.transform.position.x;
+        float y = PlayerLocal.transform.position.y;
+        BitConverter.GetBytes(x).CopyTo(buffer, 0);
+        BitConverter.GetBytes(y).CopyTo(buffer, 4);
+        mSocket.BeginSend(buffer, 0, 8, SocketFlags.None, null, null);
+    }
+
+    void UpdatePlayerRemote()
+    {
+        PlayerRemote.transform.position = new Vector3(mTargetX, mTargetY);
+    }
+}
---
+++ tcpserver/tcpserver/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpserver
+{
+    class Program
+    {
+        static System.Collections.Generic.List<Socket> mClients = new System.Collections.Generic.List<Socket>();
+
+        static void Main(string[] unused)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(new IPEndPoint(IPAddress.Any, 1337));
+            s.Listen(5);
+            while (true)
+            {
+                Socket client = s.Accept();
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                mClients.Add(client);
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(DataReceived);
+                args.SetBuffer(new byte[8], 0, 8);
+                client.ReceiveAsync(args);
+            }
+        }
+
+        static void DataReceived(object o, SocketAsyncEventArgs args)
+        {
+            Console.WriteLine();
+            Socket s = (Socket)o;
+            byte[] buffer = args.Buffer;
+        again:
+            //Console.WriteLine("Received: " + args.BytesTransferred);
+            //Console.WriteLine("Offset: " + args.Offset);
+            //Console.WriteLine("Available: " + s.Available);
+            int receivedSize = args.BytesTransferred + args.Offset;
+            if (receivedSize == 8)
+            {
+                Console.WriteLine("Received data");
+                SendToOtherClients(s, (byte[])buffer.Clone());
+                if (s.Available >= 8)
+                {
+                    s.Receive(buffer);
+                    goto again;
+                }
+                if (s.Available > 0)
+                {
+                    int received = s.Receive(buffer);
+                    args.SetBuffer(received, 8 - received);
+                }
+                else
+                {
+                    args.SetBuffer(0, 8);
+                }
+            }
+            else
+            {
+                args.SetBuffer(receivedSize, 8 - receivedSize);
+            }
+            
+            s.ReceiveAsync(args);
+        }
+
+        static void SendToOtherClients(Socket sender, byte[] msg)
+        {
+            for (int i = 0; i < mClients.Count; i++)
+            {
+                Socket client = mClients[i];
+                if (client != sender && client.Connected)
+                {
+                    try
+                    {
+                        client.BeginSend(msg, 0, 8, SocketFlags.None, null, null);
+                    } catch (Exception) {
+                        Console.WriteLine("Removing dead client");
+                        mClients.RemoveAt(i--);
+                    }
+                }
+            }
+        }
+    }
+}
